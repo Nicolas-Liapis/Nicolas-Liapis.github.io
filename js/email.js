294c8933104a0c6c101eac74a4ba5adc0b337d66
Nicolas <nicolas.liapis.dev@gmail.com>
2017-08-29 18:49:23 +0100

    v3
    final version. final check pending

--- js/email.js
+++ js/email.js
@@ -1,3 +1,10 @@
 function email() {
   document.getElementById('email').innerHTML = "nicolas.liapis.dev@gmail.com"
 }
+
+$('#email').click(function() {
+  $('#email').animate({
+    opacity: 0.25,
+    height: "toggle"
+  }, 10000, function() {});
+});
